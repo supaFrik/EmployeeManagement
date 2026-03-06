1fc9a2ea869c35d0d21054fb9261b8a40c02f5d4
supaFrik <tqv2005business@gmail.com>
2026-03-06 21:35:54 +0700

    feat(auth): add authentication provider using Spring Security

--- src/main/java/org/employee/config/SecurityConfig.java
+++ src/main/java/org/employee/config/SecurityConfig.java
@@ -1,16 +1,53 @@
 package org.employee.config;
 
 import org.employee.repository.UserRepository;
+import org.employee.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeRequests((requests) -> requests
+                .requestMatchers("/registration**", "/js/**", "/css/**", "/img/**").permitAll()
+        .anyRequest().authenticated()
+        )
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout((logout) -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll()
+                );
+        return http.build();
     }
 }
--- src/main/java/org/employee/dto/UserRegistrationDto.java
+++ src/main/java/org/employee/dto/UserRegistrationDto.java
@@ -1,23 +1,33 @@
 package org.employee.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegistrationDto {
+    @NotBlank(message = "First name is required")
+    @Size(min = 2, max = 50)
     private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    @Size(min = 2, max = 50)
     private String lastName;
+
+    @NotBlank(message = "Email is required")
+    @Size(min = 6)
     private String email;
+
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[0-9]).{6,}$",
+            message = "Password must contain uppercase letter and number"
+    )
     private String password;
-
-    public UserRegistrationDto() {}
-
-    public UserRegistrationDto(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
 }
